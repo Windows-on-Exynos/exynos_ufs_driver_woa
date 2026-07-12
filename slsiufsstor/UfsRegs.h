2017a84057b00d686e12cf37ca1429eb90b69952
viZPilot <vicenteicc2008@gmail.com>
2026-07-12 13:02:41 -0400

    add some credits for defines on UfsRegs.h

--- slsiufsstor/UfsRegs.h
+++ slsiufsstor/UfsRegs.h
@@ -4,3 +4,12 @@
 
 #pragma once
 
+//
+// I took them from Little Kernel (LK) for Exynos 9610
+//
+
+#define REG_INTERRUPT_STATUS  0x20
+#define REG_INTERRUPT_ENABLE  0x24
+#define REG_CONTROLLER_STATUS 0x30
+#define REG_CONTROLLER_ENABLE 0x34
+
